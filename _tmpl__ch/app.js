b7129399c68f75ad0ced1342fff8c138f534f866
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-12 14:30:46 -0500

    UPDATE MULTI:  DEL screenshots. ADD ch tmpl

---
+++ _tmpl__ch/app.js
@@ -0,0 +1,18 @@
+sec_num = 2
+ch_title = "CH: SOMETHING"
+
+// define elements
+const chapter = document.querySelector('h2');
+chapter.innerHTML = ch_title;
+
+const sec_el = document.querySelector('h1 > span');
+sec_el.innerHTML = sec_num
+
+/* --------------------------- */
+
+let val;
+
+
+
+
+console.log(val)
