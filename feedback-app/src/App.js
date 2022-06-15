da1964684c3535817325cae04ea9892cb65ec87a
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-15 15:43:09 -0500

    UPDATE App.js:  Demo using JS vars in JSX

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
-
 function App() {
-  return React.createElement(
-    'div',
-    { className: 'container' },
-    React.createElement('h1', {}, 'My App')
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+
+  return (
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </div>
   )
 }
 
