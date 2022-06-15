b53557ea6e331fd49c11b9e45fcc7ecb99c6ef7d
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-15 15:02:31 -0500

    UPDATE index.js:  CHANGE render to <App />

--- feedback-app/src/index.js
+++ feedback-app/src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<h1>My App</h1>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
