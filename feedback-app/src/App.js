c615acb0868f16346644eabd703439b1c8caf5aa
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-15 15:49:19 -0500

    UPDATE App.js:  Add basic array of objects and display length

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,11 +1,21 @@
 function App() {
   const title = 'Blog Post'
   const body = 'This is my blog post'
+  const comments = [
+    { id: 1, text: 'Comment one' },
+    { id: 2, text: 'Comment two' },
+    { id: 3, text: 'Comment three' },
+  ]
 
   return (
     <div className="container">
-      <h1>{title}</h1>
+      <h1>{title.toUpperCase()}</h1>
       <p>{body}</p>
+      {Math.random() * (5 + 5)}
+
+      <div className="comments">
+        <h3>Comments: ({comments.length})</h3>
+      </div>
     </div>
   )
 }
