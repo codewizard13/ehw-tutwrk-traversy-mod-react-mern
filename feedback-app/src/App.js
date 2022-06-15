396955076bd9eec49d3d8db11c77333f90278608
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-15 16:58:03 -0500

    PASS props from App.js to Header.jsx

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header'
 function App() {
   return (
     <>
-      <Header />
+      <Header text="Hello World" />
       <div className="container">
         <h1>My App</h1>
       </div>
--- feedback-app/src/components/Header.jsx
+++ feedback-app/src/components/Header.jsx
@@ -1,8 +1,8 @@
-function Header() {
+function Header(props) {
   return (
     <header>
       <div className="container">
-        <h2>Feedback UI</h2>
+        <h2>{props.text}</h2>
       </div>
     </header>
   )
