1e41c3224521645298ecf67c4662ec8d435a0e19
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-15 15:35:39 -0500

    UPDATE App.js:  How to create App without JSX (OLD way)

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,6 +1,10 @@
+import React from 'react'
+
 function App() {
-  return (
-    <h1>Hello from the app component</h1>
+  return React.createElement(
+    'div',
+    { className: 'container' },
+    React.createElement('h1', {}, 'My App')
   )
 }
 
