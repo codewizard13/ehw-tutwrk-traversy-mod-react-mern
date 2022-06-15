b50d69fcfc47b01e57550712beb909626c0f1dae
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-15 16:36:09 -0500

    UPDATE App.js:  Conditional if with logical && shorthand

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
   ]
 
   const loading = false
-  const showComments = true
+  const showComments = false
 
   if (loading) return <h1>Loading ... </h1>
 
@@ -17,16 +17,14 @@ function App() {
       <h1>{title.toUpperCase()}</h1>
       <p>{body}</p>
 
-      {showComments ? 'yes' : 'no'}
-
-      <div className="comments">
+      {showComments && (<div className="comments">
         <h3>Comments: ({comments.length})</h3>
         <ul>
           {comments.map((comment, index) => (
             <li key={index}>{comment.text}</li>
           ))}
         </ul>
-      </div>
+      </div>)}
     </div>
   )
 }
