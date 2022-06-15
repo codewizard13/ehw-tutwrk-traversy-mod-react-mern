f3156021d696fa06865dea64259ddee8585db5ce
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-15 16:40:22 -0500

    UPDATE App.js:  RESTORE to basic My App output

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,32 +1,7 @@
 function App() {
-  const title = 'Blog Post'
-  const body = 'This is my blog post'
-  const comments = [
-    { id: 1, text: 'Comment one' },
-    { id: 2, text: 'Comment two' },
-    { id: 3, text: 'Comment three' },
-  ]
-
-  const loading = false
-  const showComments = true
-
-  if (loading) return <h1>Loading ... </h1>
-
-  const commentBlock = <div className="comments">
-    <h3>Comments: ({comments.length})</h3>
-    <ul>
-      {comments.map((comment, index) => (
-        <li key={index}>{comment.text}</li>
-      ))}
-    </ul>
-  </div>
-
   return (
     <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
+      <h1>My App</h1>
     </div>
   )
 }
